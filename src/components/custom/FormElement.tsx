a4e4b7612882c4360d9853144097d15f04f1a649
masaru-suzuki <masaru.suzuki.yamagata@gmail.com>
2024-06-21 08:42:33 +0900

    add: formElementでdefault valueを受け付ける

--- src/components/custom/FormElement.tsx
+++ src/components/custom/FormElement.tsx
@@ -1,7 +1,13 @@
 import { Input } from '../ui/input';
 import { Label } from '../ui/label';
 
-export const FormElement = ({ item }: { item: string }) => {
+export const FormElement = ({
+  item,
+  data = '',
+}: {
+  item: string;
+  data?: string;
+}) => {
   return (
     <div className="grid justify-items-start gap-2">
       <Label htmlFor={item}>{item}</Label>
@@ -9,6 +15,7 @@ export const FormElement = ({ item }: { item: string }) => {
         id={item}
         name={item}
         placeholder={item}
+        defaultValue={data}
         className="border border-gray-300 rounded-md text-white-700 bg-zinc-700"
       />
     </div>
